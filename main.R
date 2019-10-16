7f538478bd6c1e69d901e5df92169dd0d514ac96
Agnieszka Choczyńska <284091@zarz.agh.edu.pl>
2019-10-16 13:00:31 +0200

    Filtering EU countries.

--- main.R
+++ main.R
@@ -1,18 +1,33 @@
+# Pakiety
+install.packages("dplyr")
+library(dplyr)
 
 # Wczytanie danych
 data <- readRDS("data.rds")
 
-str(data[,351:440])
-unique(data$V1)
+# Kody krajów UE
+country_nr <- c(40, 56, 100, 192, 203, 208, 233, 246, 350, 276, 300, 348, 380, 428, 440, 442, 372, 528, 616,
+                620, 703, 705, 724, 756, 826, 470, 642)
+country_name <- c("Austria", "Belgium", "Bulgaria", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland",
+                  "France", "Germany", "Greece", "Hungary", "Italy", "Latvia", "Lithuania", "Luxembourg",
+                  "Ireland", "Netherlands", "Poland", "Portugal", "Slovakia", "Slovenia", "Spain",
+                  "Switzerland", "Great Britain", "Malta", "Romania")
+
+countries <- cbind(country_nr, country_name)
 
 # Wybrane zmienne
 # V1 - Wave (czy jest tylko jedna?)
-# V2 - 
+# V2 - Country (nr)
+# V3 - Country_split - raczej nie ma znaczenia, skoro analiza nie jest przeprowadzana na przestrzeni czasu
+# V127 - leader - silny lider powinien rządzić
+# V128 - experts - Rządy ekspertów
+# V129 - army - Arma powinna rządzić
+# V130 - democracy - Rządy demokratyczne 
 
-library(dplyr)
 
 dem_data <- data %>% select(wave = V1, country = V2, country_split = V2A,
-                    leader = V127, experts = V128, army = V129, democracy = V130)
+                    leader = V127, experts = V128, army = V129, democracy = V130) %>%
+            filter(country %in% country_nr)
 
 # Flagi w pliku questionare
 unique(dem_data$country_split)
