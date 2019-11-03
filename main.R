69125fa6f954ac319c73e52c4bbc3bc4cbfa2289
agachocz <agachocz@gmail.com>
2019-11-03 22:06:32 +0100

    Changing data type, computing models with restrictions and testing measurement invariance.

--- main.R
+++ main.R
@@ -5,8 +5,7 @@ install.packages("lavaan")
 library(lavaan)
 install.packages("semTools")
 library(semTools)
-install.packages("semPlot")
-library(semPlot)
+
 
 # Wczytanie danych
 data <- readRDS("data.rds")
@@ -30,17 +29,52 @@ countries <- cbind(country_nr, country_name)
 # V129 - army - Arma powinna rządzić
 # V130 - democracy - Rządy demokratyczne 
 
-
-dem_data <- data %>% select(wave = V1, country = V2, country_split = V2A,
+levels <- c(1:4) # Kody ujemne oznaczają braki odpowiedzi
+# Poniżej poleceniem filter_at wybieram tylko te wiersze, które mają odpowiedź na każde pytanie
+dem_data <- data %>% select(wave = V1, country = V2,
                     leader = V127, experts = V128, army = V129, democracy = V130) %>%
-            filter(country %in% country_nr)
+            filter(country %in% country_nr) %>%
+            filter_at(vars(leader, experts, army, democracy), all_vars(. %in% levels))
 
-# Flagi w pliku questionare
-unique(dem_data$country_split)
-unique(dem_data$country)
+head(dem_data)
 
+# Wstępna analiza korelacji
+cor(dem_data[,3:6], method="spearman")
+# Democracy jest ujemnie skorelowana z wszystkimi pozostałymi
+# Niedemokratyczne poglądy - chociaż w praktyce też nie da się ich łącznie zastosować -
+# częściej występują razem
+# Najsilniej skorelowane są army oraz leader
+
+
+dem_data$leader = factor(dem_data$leader, ordered = T)
+dem_data$experts = factor(dem_data$experts, ordered = T)
+dem_data$army = factor(dem_data$army, ordered = T)
+dem_data$democracy = factor(dem_data$democracy, ordered = T)
+
+# Struktura zmiennej latentnej
 model <- 'dem_model =~ leader + experts + army + democracy'
+or.vars <- c("leader", "experts", "army", "democracy")
 
-fit <- cfa(model, data = dem_data)
-summary(fit, standardized = TRUE, fit.measures = TRUE)
+
+# Dopasowanie modelu CFA
+
+fit <- cfa(model, data = dem_data, ordered=or.vars)
+summary(fit, standardized = TRUE, fit.measures = TRUE, rsquare = TRUE)
+
+# Modele do testowania równoważności pomiarowej
+
+configural <- cfa(model, data=dem_data, ordered=or.vars, group="country")
+weak <- cfa(model, data=dem_data, ordered=or.vars, group="country", group.equal="loadings")
+strong <- cfa(model, data=dem_data, ordered=or.vars, group="country", group.equal=c("loadings", "intercepts"))
+
+# Dodatkowy poziom, zwykle pomijany, bo nie jest konieczny do badań porównawczych
+strict <- cfa(model, data=dem_data, group="country", group.equal=c("loadings", "intercepts", "residuals"))
+
+#Test
+
+anova(configural, weak)
+# P-value bardzo małe, z czego by wynikało, że słaba równoważność nie jest spełniona
+
+modelsCat <- list(fit.configural = configural, fit.loadings = weak)
+partialInvarianceCat(modelsCat, type = "metric")
 
